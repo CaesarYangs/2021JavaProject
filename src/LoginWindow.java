e9b48de63def4e3712849c348c2a0144a240c6a8
CaesarYang <CaesarYangs@outlook.com>
2021-03-31 08:48:55 +0800

    1

--- src/LoginWindow.java
+++ src/LoginWindow.java
@@ -8,19 +8,30 @@ import java.awt.event.ActionListener;
 
 public class LoginWindow {
     public static void LoginW(JFrame relativeWindow) {
-        JFrame newJFrame = new JFrame("新的窗口");
+        JFrame newJFrame = new JFrame("登陆");
 
-        newJFrame.setSize(250, 250);
+        newJFrame.setSize(700, 400);
 
         // 把新窗口的位置设置到 relativeWindow 窗口的中心
         newJFrame.setLocationRelativeTo(relativeWindow);
 
         // 点击窗口关闭按钮, 执行销毁窗口操作（如果设置为 EXIT_ON_CLOSE, 则点击新窗口关闭按钮后, 整个进程将结束）
-        newJFrame.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+        newJFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 
+        JPanel panel = new JPanel(new FlowLayout());
+
+        JButton login = new JButton("登陆");
+        final JButton register = new JButton("测试按钮");
+
+        login.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                MainMenu.MainWindow(newJFrame);
+            }
+        });
 
         // 第 1 个 JPanel, 使用默认的浮动布局
-        JPanel panel01 = new JPanel();
+        /*JPanel panel01 = new JPanel();
         panel01.add(new JLabel("用户名"));
         panel01.add(new JTextField(10));
 
@@ -30,20 +41,25 @@ public class LoginWindow {
         panel02.add(new JPasswordField(10));
 
         // 第 3 个 JPanel, 使用浮动布局, 并且容器内组件居中显示
-        JPanel panel03 = new JPanel(new FlowLayout(FlowLayout.CENTER));
-        panel03.add(new JButton("登录"));
-        panel03.add(new JButton("注册"));
+        JPanel panel03 = new JPanel(new FlowLayout(FlowLayout.CENTER));*/
+
+
+        //panel03.add(new JButton("登录"));
+        //panel03.add(new JButton("注册"));
+
+        panel.add(login);
+        panel.add(register);
+
 
         // 创建一个垂直盒子容器, 把上面 3 个 JPanel 串起来作为内容面板添加到窗口
-        Box vBox = Box.createVerticalBox();
+        /*Box vBox = Box.createVerticalBox();
         vBox.add(panel01);
         vBox.add(panel02);
         vBox.add(panel03);
 
-        newJFrame.setContentPane(vBox);
+        newJFrame.setContentPane(vBox);*/
 
-        newJFrame.pack();
-        newJFrame.setLocationRelativeTo(null);
+        newJFrame.setContentPane(panel);
         newJFrame.setVisible(true);
     }
 }
---
+++ src/MainMenu.java
@@ -0,0 +1,50 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class MainMenu {
+    public static void MainWindow(JFrame relativeWindow){
+        JFrame Mmenu = new JFrame("MainMenu");
+
+        Mmenu.setSize(1500, 900);
+
+        // 把新窗口的位置设置到 relativeWindow 窗口的中心
+        Mmenu.setLocationRelativeTo(relativeWindow);
+
+        // 点击窗口关闭按钮, 执行销毁窗口操作（如果设置为 EXIT_ON_CLOSE, 则点击新窗口关闭按钮后, 整个进程将结束）
+        Mmenu.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+
+        JPanel panel = new JPanel(new FlowLayout());
+
+        JButton btn01 = new JButton("按钮01");
+        JButton btn02 = new JButton("按钮02");
+        JButton btn03 = new JButton("按钮03");
+        JButton btn04 = new JButton("按钮04");
+        JButton btn05 = new JButton("按钮05");
+        JButton btn = new JButton("Show New Window");
+        btn.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                // 点击按钮, 显示新的一个窗口
+                LoginWindow.LoginW(Mmenu);
+            }
+        });
+
+
+
+        panel.add(btn01);
+        panel.add(btn02);
+        panel.add(btn03);
+        panel.add(btn04);
+        panel.add(btn05);
+        panel.add(btn);
+
+
+
+        Mmenu.setContentPane(panel);
+        Mmenu.setVisible(true);        // PS: 最后再设置为可显示(绘制), 所有添加的组件才会显示
+
+
+    }
+}
